04e70a95f0a5a6d930a91714d66bbbce67f1a514
MillieKS <mkseverino95@gmail.com>
2022-08-12 13:44:32 +0100

    checked tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,13 @@
-require 'fizzbuzz'
+require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" for the number 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" for  the number 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'return "buzz" when passed 15' do 
+  it 'return "buzz" for  the number 15' do 
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end 
   it 'returns 2 when passed 2' do
